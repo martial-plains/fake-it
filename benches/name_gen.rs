7195aec9c1f59047dc9fa2524ba2d741c2a7c410
Allister Isaiah Harvey <41553768+powpow58@users.noreply.github.com>
2022-04-03 19:06:19 -0400

    Create benches for name generating functions

---
+++ benches/name_gen.rs
@@ -0,0 +1,17 @@
+#![feature(test)]
+extern crate test;
+
+use fake_it::person::Person;
+use test::Bencher;
+
+#[bench]
+fn bench(b: &mut Bencher) {
+    b.iter(|| create_100_random_person_names());
+}
+
+fn create_100_random_person_names() {
+    for _ in 0..100 {
+        let names = Person::gen_rand_fname();
+        let surnames = Person::gen_rand_lname();
+    }
+}
