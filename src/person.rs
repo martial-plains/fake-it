4bda6897a3396a5cdaad07aa0fa2c59b1d956299
Allister Isaiah Harvey <41553768+powpow58@users.noreply.github.com>
2022-04-06 04:31:57 -0400

    Add Gender to Person object

--- src/person.rs
+++ src/person.rs
@@ -2,13 +2,44 @@ use chrono::{DateTime, Datelike, TimeZone, Utc};
 use rand::Rng;
 use serde::{Deserialize, Serialize};
 
-use crate::{datetime, providers};
+use crate::{
+    datetime,
+    providers::{
+        self,
+        person::{first_names_female, first_names_male},
+    },
+};
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub enum Gender {
+    Male,
+    Female,
+}
+
+impl Gender {
+    fn by_name(name: &str) -> Gender {
+        let female_fnames = first_names_female();
+        let male_fnames = first_names_male();
+        match name {
+            n if female_fnames.contains(&n) && male_fnames.contains(&n) => {
+                match rand::random::<bool>() {
+                    true => Gender::Male,
+                    false => Gender::Female,
+                }
+            }
+            n if female_fnames.contains(&n) => Gender::Female,
+            _ => Gender::Male,
+        }
+    }
+}
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct Person {
     first_name: String,
     surname: String,
     date_of_birth: DateTime<Utc>,
+    gender: Gender,
+    spouse: Option<Box<Person>>,
 }
 
 impl Person {
@@ -31,6 +62,16 @@ impl Person {
     }
 
     #[must_use]
+    pub fn spouse(&self) -> Option<&Person> {
+        self.spouse.as_ref().map(|p| p.as_ref())
+    }
+
+    #[must_use]
+    pub fn is_married(&self) -> bool {
+        self.spouse.is_some()
+    }
+
+    #[must_use]
     /// Geerates random female name
     pub fn gen_rand_fname() -> &'static str {
         let mut rng = rand::thread_rng();
@@ -50,8 +91,10 @@ impl Person {
 impl Default for Person {
     fn default() -> Person {
         let mut rng = rand::thread_rng();
+        let first_name = Person::gen_rand_fname().to_string();
+        let gender = Gender::by_name(&first_name);
         Person {
-            first_name: Person::gen_rand_fname().to_string(),
+            first_name,
             surname: Person::gen_rand_lname().to_string(),
             date_of_birth: {
                 let d = Utc::now().date();
@@ -73,6 +116,39 @@ impl Default for Person {
                     datetime::get_random_date_in_month(year, month)
                 }
             },
+            spouse: None,
+            gender,
         }
+    }
+}
+
+impl std::fmt::Display for Person {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.gender {
+            Gender::Male => {
+                write!(f, "{} {}", self.first_name(), self.surname())
+            }
+            Gender::Female => match self.is_married() {
+                true => write!(f, "{} {} {}", "Mrs.", self.first_name(), self.surname()),
+                false => write!(f, "{} {}", self.first_name(), self.surname()),
+            },
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_johnathon_is_male() {
+        let result = Gender::by_name("Johnathon");
+        assert!(result == Gender::Male)
+    }
+
+    #[test]
+    fn test_kristy_is_female() {
+        let result = Gender::by_name("Kristy");
+        assert!(result == Gender::Female)
     }
 }
